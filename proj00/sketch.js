99e7a6f753be48e6eba9a9ed022ca61b6fe50dba
enricozb <enricozb@gmail.com>
2014-09-29 22:23:17 -0500

    Fixed Github Nonsense

--- proj00/sketch.js
+++ proj00/sketch.js
@@ -4,15 +4,12 @@ function setup()
 }
 function draw() 
 {
-<<<<<<< HEAD:Test/proj00/sketch.js
 	background(255);
 	rect(mouseX,mouseY,80,80);
 }
-=======
 	push();
 	fill(100,100);
 	rect(0,0,width,height);
 	pop();
 	ellipse(mouseX,mouseY,100,100);
 }
->>>>>>> FETCH_HEAD:proj00/sketch.js

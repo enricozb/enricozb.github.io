b15e00981634040f0216b13e3c45598b8eac4c6a
enricozb <Enrico@Enricos-MBP.home>
2014-10-08 23:55:00 -0500

    tsd

--- proj00/sketch.js
+++ proj00/sketch.js
@@ -1,18 +1,15 @@
 function setup()
 {
-	createCanvas(640,1136);
+	createCanvas(640,1136)
 }
 function draw() 
 {
-<<<<<<< HEAD:Test/proj00/sketch.js
-	background(255);
-	rect(mouseX,mouseY,80,80);
+	background(255)
+	rect(mouseX,mouseY,80,80)
 }
-=======
-	push();
-	fill(100,100);
-	rect(0,0,width,height);
-	pop();
-	ellipse(mouseX,mouseY,100,100);
+	push()
+	fill(100,100)
+	rect(0,0,width,height)
+	pop()
+	ellipse(mouseX,mouseY,100,100)
 }
->>>>>>> FETCH_HEAD:proj00/sketch.js

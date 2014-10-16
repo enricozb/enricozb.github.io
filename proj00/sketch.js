b889a3f03894a8657c614341cb7794f599c24120
enricozb <Enrico@Enricos-MBP.home>
2014-10-16 00:14:38 -0500

    Older version of Processing.js added (p4 folder)

--- proj00/sketch.js
+++ proj00/sketch.js
@@ -1,26 +1,53 @@
-function setup()
-{
-	createCanvas(640,1136)
-}
-function draw() 
-{
-<<<<<<< HEAD
-	background(255)
-	rect(mouseX,mouseY,80,80)
-}
-	push()
-	fill(100,100)
-	rect(0,0,width,height)
-	pop()
-	ellipse(mouseX,mouseY,100,100)
-=======
+int s = 1000;
+
+Walker[] w = new Walker[s];
+
+float a = 250;
+float b = 250;
+float t = 0;
+void setup() {
+	size(500,500,P3D);
+	sphereDetail(5);
 	background(255);
-	rect(mouseX,mouseY,80,80);
+	stroke(0);
+	fill(0);
+	for(int i = 0; i < s; i++)
+	{
+		w[i] = new Walker(0,0,0);
+	}
+	smooth(8);
+	fill(255);
+	stroke(255);
 }
-	push();
-	fill(100,100);
-	rect(0,0,width,height);
-	pop();
-	ellipse(mouseX,mouseY,100,100);
->>>>>>> FETCH_HEAD
+
+float xr = 0; //X-rotation
+float yr = 0; //Y-rotation
+void draw() {
+	background(0);
+	translate(250,250,250);
+	rotateY(noise(t));
+	rotateX(noise(t + 100));
+	rotateZ(noise(t + 10));
+	if(pmouseX != mouseX || pmouseY != mouseY)
+	{
+		t += .01;
+	}
+	rotateY(-radians(xr));
+	rotate (PI/2 - radians(yr), sin(PI/2+radians(xr)), 0, cos(PI/2+radians(xr)));
+	for(Walker p : w)
+	{
+		p.update();
+	}
+}
+
+void keyPressed()
+{
+	if(keyCode == UP)
+		yr++;
+	else if(keyCode == DOWN)
+		yr--;
+	else if(keyCode == RIGHT)
+		xr++;
+	else if(keyCode == LEFT)
+		xr--;
 }

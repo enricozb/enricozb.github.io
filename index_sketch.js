af02d2488714b0da334f69f0e01e1f9de73e0577
enricozb <Enrico@Enricos-MBP.home>
2014-11-18 19:58:09 -0600

    added mp3 file, preparing sexy stuff

---
+++ index_sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(window.innerWidth, window.innerHeight)
+}
+
+function draw() {
+	translate(window.innerWidth/2,window.innerHeight/2)
+}
+
+function keyPressed() {
+
+}

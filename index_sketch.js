f0aded43d3bf43a7e5cb055860e07f40eab361f5
enricozb <Enrico@Enricos-MBP.home>
2014-11-18 20:14:10 -0600

    Testing buzz

--- index_sketch.js
+++ index_sketch.js
@@ -1,9 +1,14 @@
+var audio = new buzz.sound("assets/aphex.mp3");
+
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight)
+	//var audio = loadSound('assets/aphex.mp3')
+	audio.play();
+	//osc.start()
 }
 
 function draw() {
-	translate(window.innerWidth/2,window.innerHeight/2)
+	ellipse(pwinMouseX,pwinMouseY,50,50)
 }
 
 function keyPressed() {

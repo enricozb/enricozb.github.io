012324b914340869bcddd8198536152891ae956f
enricozb <Enrico@Enricos-MBP.home>
2014-11-18 20:53:57 -0600

    testpush

--- index_sketch.js
+++ index_sketch.js
@@ -1,14 +1,34 @@
-var audio = new buzz.sound("assets/aphex.mp3");
+var audio
+var peaks
+var PEAKLENGTH = 100000
+var bufferMax = 500
+function preload() {
+	audio = loadSound('assets/aphex.mp3')
+}
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight)
-	//var audio = loadSound('assets/aphex.mp3')
-	audio.play();
-	//osc.start()
+
+	colorMode(HSB, 255)
+	rectMode(CENTER)
+	noStroke()
+
+	audio.play()
+	peaks = audio.getPeaks(PEAKLENGTH)
 }
 
 function draw() {
-	ellipse(pwinMouseX,pwinMouseY,50,50)
+	var amp = audio.getLevel()
+	var uamp = map(amp,0,1,0,400)
+	var ctime = ~~(audio.currentTime()/audio.duration() * PEAKLENGTH)
+
+	background(uamp,100,100)
+	translate(0,window.innerHeight/2)
+	rect(window.innerWidth/2,0,window.innerHeight,10)
+	//text(ctime, 200,200)
+	for (var i = 0; i < bufferMax; i++) {
+		rect(map(i,0,bufferMax,0,window.innerWidth),map(peaks[i + ctime], 0, 1, 0,200),2,2)
+	}
 }
 
 function keyPressed() {

aa98123ba33f229c3db131c9b2fd1de9c0851970
enricozb <Enrico@Enricos-MBP.home>
2014-11-25 13:18:31 -0600

    accurate colors (blue)

--- index_sketch.js
+++ index_sketch.js
@@ -1,58 +1,47 @@
 var audio
 var peaks
-var PADDING = window.innerWidth/10
-var PEAKLENGTH = 1000000
-var bufferMax = 200
-var first = true
-var time = 0.0
-var s = 2
+
+var PADDING
+var PEAKLENGTH
+var SCALE
+var bufferMax
+var doneLoading
+var time
+var s
+
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight)
 
 	colorMode(HSB, 255)
 	rectMode(CENTER)
-	//strokeWeight(5)
-	noStroke()
-	audio = new p5.SoundFile('assets/flim.mp3',playSound)
-	//audio.play()
+	stroke(127.5,100,200)
+
+	PADDING = window.innerWidth/10
+	PEAKLENGTH = 1000000
+	SCALE = window.innerHeight/7
+	bufferMax = 100
+	doneLoading = false
+	time = 0.0
+	s = 2
+
+	audio = new p5.SoundFile('assets/flim.mp3',play)
 }
 
-function playSound() {
-	//var osc = new p5.Oscillator(440, 'sine')
-	//osc.start()
+function play() {
 	audio.play()
+	peaks = audio.getPeaks(PEAKLENGTH)
+	doneLoading = true
 }
 
 function drawLoad() {
 	background(0,100,100)
 	translate(0,window.innerHeight/2)
-	stroke(0,100,200)
 	line(PADDING, 0, PADDING + time, 0)
-
-	/*
-	translate(window.innerWidth/2, window.innerHeight/2)
-	for(var i = 0; i < 2*PI; i += radians(10))
-	{
-		fill(map((time + i + PI)%(PI*2), 0, 2 * PI, 0, 255), 200, 200)
-		var w = map((i + PI)%(PI*2), 0, 2 * PI, 0, 100) * s
-		push()
-		translate(s * 100 * cos(i + sin(time + i)),s * 100 * sin(i + sin(time + i)))
-		rotate(i + sin(time + i))
-		rect(0,0,w,5 * s)
-		pop()
-	}
-	time += .07;
-	*/
 	time += 10;
 }
 
 function draw() {
-	if(audio.isLoaded() && first)
-	{
-		peaks = audio.getPeaks(PEAKLENGTH)
-		first = false
-	}
-	else if(first)
+	if(!doneLoading)
 	{
 		drawLoad()
 		return;
@@ -62,25 +51,12 @@ function draw() {
 	var ctime = ~~(audio.currentTime()/audio.duration() * PEAKLENGTH)
 	background(uamp,100,100)
 	translate(0,window.innerHeight/2)
-	strokeWeight(1)
-	//text(ctime, 200,200)
 	beginShape()
 	for (var i = 0; i < bufferMax; i++) {
-		//fill(map(peaks[i + ctime], 0, 1, 0,400),200,200)
-		//noStroke()
-		stroke(map(peaks[i + ctime], 0, 1, 0,400),100,200)
-		//rect(map(i,0,bufferMax,PADDING,window.innerWidth - PADDING),map(peaks[i + ctime], 0, 1, 0,200),3,3)
-	
-		//beginShape()
-		//curveVertex(map(i,0,bufferMax,0,window.innerWidth),map(peaks[i + ctime-1], 0, 1, 0,200))
-		//curveVertex(map(i,0,bufferMax,0,window.innerWidth),map(peaks[i + ctime-1], 0, 1, 0,200))
-		//curveVertex(map(i,0,bufferMax,0,window.innerWidth),map(peaks[i + ctime], 0, 1, 0,200))
-		curveVertex(map(i,0,bufferMax,PADDING,window.innerWidth-PADDING),map(peaks[i + ctime], 0, 1, 0,200))
-	//endShape()
+		stroke(abs(map(peaks[i + ctime], 0, 1, 0,400)) + 127.5,100,200)
+		if(i == 0 || i == bufferMax - 1)
+			curveVertex(map(i,0,bufferMax,PADDING,window.innerWidth-PADDING),map(peaks[i + ctime], 0, 1, 0,SCALE))
+		curveVertex(map(i,0,bufferMax,PADDING,window.innerWidth-PADDING),map(peaks[i + ctime], 0, 1, 0,SCALE))
 	}
 	endShape()
-}
-
-function keyPressed() {
-
 }
